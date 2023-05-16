2a994638f2c87a6aebe83cf31310b558fd2c206c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-16 11:49:30 -1100

    Add the dragon component

---
+++ src/components/Dragons.js
@@ -0,0 +1,12 @@
+export default function Dragons() {
+  return (
+    <figure>
+      <img src="" alt="dragon 1" />
+      <div>
+        <figcaption>Dragon 1</figcaption>
+        <p>Description</p>
+        <button type="button">Reserve Dragon</button>
+      </div>
+    </figure>
+  );
+}
