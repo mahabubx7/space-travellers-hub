9ddc63a1b4e51c7b527ce97cf3fd6f5d7d47cc7f
touredri <dcompte223@gmail.com>
2023-05-18 00:28:38 +0100

    display join on MyProfile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -6,17 +6,18 @@ const MyProfile = () => {
   const data = rockets.data.filter((rocket) => rocket.reserved === true);
   const { value } = useSelector((state) => state.dragons);
   const reservedDragons = value.filter((item) => item.reserved);
+  const Mission = useSelector((store) => store.missions);
+  const missionData = Mission.filter((elm) => elm.reserved === true);
 
   return (
     <div className="profile-page">
       <div className="missions info-card">
         <h3>my missions</h3>
-        <ul className="profile-list">
-          <li>Telstar</li>
-          <li>SES</li>
-          <li>AsiaSat</li>
-          <li>ABS</li>
-        </ul>
+        {missionData.length > 0 ? (
+          <ul className="profile-list">
+            {missionData.map((mission) => <li key={mission.id}>{mission.name}</li>)}
+          </ul>
+        ) : (<p className="profile-list">No Mission join yet!</p>)}
       </div>
       <div className="rockets info-card">
         <h3>my rockets</h3>
