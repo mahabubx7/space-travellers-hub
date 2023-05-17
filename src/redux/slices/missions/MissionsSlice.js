08aab5a825adff10cb8640dbed843e4ab7091fa2
touredri <dcompte223@gmail.com>
2023-05-17 14:57:51 +0000

    implement join button

--- src/redux/slices/missions/MissionsSlice.js
+++ src/redux/slices/missions/MissionsSlice.js
@@ -6,10 +6,24 @@ const initialState = [];
 const Missions = createSlice({
   name: 'Missions',
   initialState,
-  reducers: {},
+  reducers: {
+    join: (state, action) => {
+      const newState = state.map((elm) => {
+        if (elm.id === action.payload) {
+          return { ...elm, reserved: true };
+        }
+        return elm;
+      });
+      return newState;
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(getMissions.fulfilled, (state, action) => action.payload);
+    builder.addCase(getMissions.fulfilled, (state, action) => action.payload.map((elm) => ({
+      ...elm,
+      reserved: false,
+    })));
   },
 });
 
 export default Missions.reducer;
+export const { join } = Missions.actions;
