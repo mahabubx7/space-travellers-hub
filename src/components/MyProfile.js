7f5e042e2de6e632147a93ec67834a452cbcc610
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-17 13:16:54 -1100

    Add reserved dragons in profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styles from './MyProfile.module.css';
 
-export const MyProfile = () => (
-  <div className="profile-page">
-    <p>my profile page</p>
-  </div>
+export const MyProfile = () => {
+  const { value } = useSelector((state) => state.dragons);
+  const reservedDragons = value.filter((item) => item.reserved);
+
+  return (
+    <div className="profile-page">
+      <section>
+        <h2>My Dragons</h2>
+        <table className={styles.table}>
+          {reservedDragons.map((item) => (
+            <tr key={item.id}><td className={styles.list}>{item.name}</td></tr>
+          ))}
+        </table>
+      </section>
+    </div>
+  );
   // <div className="profile-page">
   //   <div className="missions info-card">
   //     <h3>my missions</h3>
@@ -23,6 +37,6 @@ export const MyProfile = () => (
   //     </ul>
   //   </div>
   // </div>
-);
+};
 
 export default MyProfile;
