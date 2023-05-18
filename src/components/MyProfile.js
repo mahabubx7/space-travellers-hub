c51470c84ea3608b3bf03d2fdb8c52dc0b68c5d8
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-17 14:43:38 -1100

    Update my profile component

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from './MyProfile.module.css';
 
-export const MyProfile = () => {
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const data = rockets.data.filter((rocket) => rocket.reserved === true);
   const { value } = useSelector((state) => state.dragons);
   const reservedDragons = value.filter((item) => item.reserved);
 
   return (
     <div className="profile-page">
+      <div className="missions info-card">
+        <h3>my missions</h3>
+        <ul className="profile-list">
+          <li>Telstar</li>
+          <li>SES</li>
+          <li>AsiaSat</li>
+          <li>ABS</li>
+        </ul>
+      </div>
+      <div className="rockets info-card">
+        <h3>my rockets</h3>
+        {data.length > 0 ? (
+          <ul className="profile-list">
+            {data.map((rocket) => <li key={rocket.id}>{rocket.name}</li>)}
+          </ul>
+        ) : (<p>Empty rocket reservation!</p>)}
+      </div>
       <section>
         <h2>My Dragons</h2>
         <table className={styles.table}>
@@ -18,25 +37,6 @@ export const MyProfile = () => {
       </section>
     </div>
   );
-  // <div className="profile-page">
-  //   <div className="missions info-card">
-  //     <h3>my missions</h3>
-  //     <ul className="profile-list">
-  //       <li>Telstar</li>
-  //       <li>SES</li>
-  //       <li>AsiaSat</li>
-  //       <li>ABS</li>
-  //     </ul>
-  //   </div>
-  //   <div className="rockets info-card">
-  //     <h3>my rockets</h3>
-  //     <ul className="profile-list">
-  //       <li>Falcon 9</li>
-  //       <li>Falcon Heavy</li>
-  //       <li>Starship</li>
-  //     </ul>
-  //   </div>
-  // </div>
 };
 
 export default MyProfile;
