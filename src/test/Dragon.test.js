e4adc524b52e42857bc5b1c59da02e6cc76bb202
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-17 13:18:48 -1100

    Add test for component render

---
+++ src/test/Dragon.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import Dragon from '../components/Dragon';
+
+describe('dragons ', () => {
+  it('renders dragons component', () => {
+    render(
+      <Provider store={store}>
+        <Dragon
+          name="Dragon 1"
+          reserved={false}
+          description="Hello"
+          image="dragon 1"
+          id="item 1"
+        />
+      </Provider>,
+    );
+    screen.debug();
+    expect(screen.getByText('Dragon 1')).toBeInTheDocument();
+  });
+});
