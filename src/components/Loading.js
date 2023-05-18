e928f419016950da35e9e7836d3a0076a20bbb07
Mahabub Alam <mahabubx7@gmail.com>
2023-05-18 19:21:29 +0600

    :octocat: tdd-issue-fix/rocket-test

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import icon from '../imgs/planet1.png';
 
 const Loading = () => (
-  <div className="loading">
+  <div className="loading" data-testid="loading">
     <span className="load-spin">
       <img src={icon} alt="spin-icon" />
     </span>
