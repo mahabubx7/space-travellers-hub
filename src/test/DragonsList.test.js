693acf03585eb87b1b7a692ee6f20d0662d1e70f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-19 09:24:21 -1100

    Refactor code

--- src/test/DragonsList.test.js
+++ src/test/DragonsList.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { useSelector, useDispatch } from 'react-redux';
 import DragonsList from '../components/DragonsList';
